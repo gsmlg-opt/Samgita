d055ed294c1b318ca2d6a1c30dd6fd1ef4d346d7
Jonathan Gao <gsmlg.com@gmail.com>
2026-09-04 01:38:49 +0800

    chore(devenv): update nixpkgs inputs

--- devenv.nix
+++ devenv.nix
@@ -6,7 +6,6 @@
   ...
 }: let
   pkgs-stable = import inputs.nixpkgs-stable {system = pkgs.stdenv.system;};
-  pkgs-unstable = import inputs.nixpkgs-unstable {system = pkgs.stdenv.system;};
 in {
   env.GREET = "Samgita";
   env.MIX_BUN_PATH = lib.getExe pkgs-stable.bun;
@@ -43,7 +42,10 @@ in {
       {name = "samgita_dev";}
       {name = "samgita_test";}
     ];
-    settings.unix_socket_directories = "${config.env.DEVENV_STATE}/postgres";
+    settings = {
+      port = lib.mkForce 5433;
+      unix_socket_directories = "${config.env.DEVENV_STATE}/postgres";
+    };
   };
 
   processes.samgita = {
